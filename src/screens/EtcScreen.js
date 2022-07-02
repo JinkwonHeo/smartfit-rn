0be201fa6be3206830ae854e489bcc3211a33001
JinkwonHeo <hjinkwon912@gmail.com>
2022-07-02 14:48:06 +0900

    feat/1002-1: 로그아웃 구현

--- src/screens/EtcScreen.js
+++ src/screens/EtcScreen.js
@@ -1,10 +1,41 @@
-import { View, Text } from 'react-native';
+import { View, Text, Button, StyleSheet } from 'react-native';
 import React from 'react';
+import { UserAuth } from '../context/AuthContext';
+import { auth } from '../../firebase';
+import { signOut } from 'firebase/auth';
+import { theme } from '../../theme';
+
+const colors = theme.colors;
 
 export default function EtcScreen() {
+  const { setCurrUser } = UserAuth();
+
+  const handleSignOut = async () => {
+    await signOut(auth);
+    setCurrUser(null);
+  };
   return (
-    <View style={{ marginTop: 40 }}>
-      <Text>EtcScreen</Text>
+    <View style={styles.container}>
+      <View style={styles.buttonWrapper}>
+        <Button
+          title="Sign out"
+          color={colors.secondary}
+          onPress={handleSignOut}
+        />
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  buttonWrapper: {
+    flex: 1,
+    marginTop: 'auto',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
